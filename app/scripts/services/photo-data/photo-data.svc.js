2020cc4b0df9f181d2c40083bf127ba1c58ea9af
Cristian Poll <cpoll24@gmail.com>
2016-10-05 22:21:22 -0400

    add prev/next functionality to expander; attempt to scroll to selected

--- app/scripts/services/photo-data/photo-data.svc.js
+++ app/scripts/services/photo-data/photo-data.svc.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('photoData', [])
-.service('photoDataService', ['$http', function ($http) {
+.service('photoDataService', ['$http', '$filter', '$location', '$anchorScroll', function ($http, $filter, $location, $anchorScroll) {
     this.constructor = function() {
         var t = this;
         $http.get('data/photos.json')
         .then(function(result){  
             t.data = result.data;
-            t.currentCategory = t.data.categories[0];
+            t.switchCategory(t.data.categories[0]);
             t.selectedPhoto = t.data.photos;
             t.expanderVisible = false;
         });
@@ -16,6 +16,8 @@ angular.module('photoData', [])
 
     this.switchCategory = function switchCategory(category){
         this.currentCategory = category;
+        this.photosInCategory = $filter('filter')(this.data.photos, {category:this.currentCategory});
+        console.log(this.photosInCategory);
     };
 
     this.switchPhoto = function switchPhoto(photo){
@@ -25,5 +27,30 @@ angular.module('photoData', [])
 
     this.hideExpander = function hideExpander(){
         this.expanderVisible = false;
+        $location.hash('photo-' + this.getCurrentPhotoIndex());
+        $anchorScroll();
     };
+
+    this.nextPhoto = function nextPhoto(){
+        let newPhotoIndex = this.getCurrentPhotoIndex() + 1;
+        if(newPhotoIndex >= this.photosInCategory.length) { 
+            newPhotoIndex = 0; 
+        }
+        this.switchPhoto(this.photosInCategory[newPhotoIndex]);
+    };
+
+    this.previousPhoto = function previousPhoto(){
+        let newPhotoIndex = this.getCurrentPhotoIndex() - 1;
+        if(newPhotoIndex < 0) { 
+            newPhotoIndex = this.photosInCategory.length - 1; 
+        }
+        this.switchPhoto(this.photosInCategory[newPhotoIndex]);
+    };
+
+
+    //Private
+    this.getCurrentPhotoIndex = function getCurrentPhotoIndex() {
+        return this.photosInCategory.indexOf(this.selectedPhoto);
+    };
+
 }]);
