313d89db04b331b535a2a020a53cce0f83ccd120
Cristian Poll <cpoll24@gmail.com>
2017-06-15 21:21:41 -0400

    add s3 deployment to circle.yml

---
+++ deploy_staging.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#http://benlopatin.com/deploying-static-sites-circle-ci/
+DEFAULT="default_aws_profile"
+PROFILE=${AWS_PROFILE:-$DEFAULT}
+BUCKET="ambernaytrotter.com"
+SUBDIR="staging2"
+DIR="dist/"
+aws  s3  sync $DIR s3://$BUCKET/$STAGING --profile "$PROFILE"
