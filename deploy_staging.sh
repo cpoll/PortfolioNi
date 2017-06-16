8cf08db9f07aac4f3e47f0d699341f0e40f96fb5
Cristian Poll <cpoll24@gmail.com>
2017-06-15 23:56:30 -0400

    remove --profile from s3 upload

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -6,5 +6,5 @@ BUCKET="ambernaytrotter.com"
 SUBDIR="staging/staging2"
 DIR="dist/"
 BUCKET="s3://$BUCKET/$STAGING"
-echo "syncing to $BUCKET with profile $PROFILE"
-aws s3 sync $DIR $BUCKET --profile "$PROFILE"
+echo "syncing to $BUCKET"
+aws s3 sync $DIR $BUCKET
